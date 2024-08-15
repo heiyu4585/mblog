50a6c64798f6cf9e1686c9731fd60915e578d47f
wangning <wangning4567@163.com>
2024-08-15 13:19:48 +0800

    view部分init

--- blog-view/src/plugins/axios.js
+++ blog-view/src/plugins/axios.js
@@ -3,7 +3,7 @@ import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
 
 const request = axios.create({
-	baseURL: 'http://localhost:8090/',
+	baseURL: '/api',
 	timeout: 10000,
 })
 
--- blog-view/vue.config.js
+++ blog-view/vue.config.js
@@ -1,4 +1,20 @@
 module.exports = {
+	devServer: {
+		port: "8082",
+		open: true,
+		overlay: {
+			warnings: false,
+			errors: true
+		},
+		proxy: {
+			'/api': {
+				target: 'http://localhost:8080/',
+				pathRewrite: {'^/api' : ''},
+				changeOrigin: true,     // target是域名的话，需要这个参数，
+				secure: false,          // 设置支持https协议的代理
+			}
+		}
+	},
 	configureWebpack: {
 		resolve: {
 			alias: {
