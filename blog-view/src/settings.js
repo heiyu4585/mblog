2d103e6547187b49105b696bdeb88eb1c8c59959
wangning <wangning4567@163.com>
2024-08-20 14:31:30 +0800

    view接口联调

--- blog-view/src/settings.js
+++ blog-view/src/settings.js
@@ -3,13 +3,54 @@ module.exports = {
 	 * @type {string}
 	 * @description 首页三张背景图
 	 */
-	bg1: 'https://cdn.naccl.top/blog/img/bg1.jpg',
-	bg2: 'https://cdn.naccl.top/blog/img/bg2.jpg',
-	bg3: 'https://cdn.naccl.top/blog/img/bg3.jpg',
+	bg1: 'https://cdn.naccl.top/blog/img/bg2.jpg',
+	bg2: 'https://cdn.naccl.top/blog/img/bg3.jpg',
 
 	/**
 	 * @type {string}
 	 * @description 首页故障风文字
 	 */
-	malfunctionText: 'Naccl\'s Blog'
+	malfunctionText: 'BlackRain\'s Blog',
+	defaultSiteInfo: {
+		"reward": "/img/reward.jpg",
+		"copyright": {
+			"title": "Copyright © 2019 - 2024",
+			"siteName": "blackRain'S BLOG"
+		},
+		"blogName": "blackRain's Blog",
+		"beian": "",
+		"webTitleSuffix": " - blackRain's Blog",
+		"footerImgTitle": "手机看本站",
+		"commentAdminFlag": "咕咕",
+		"footerImgUrl": "/img/qr.png"
+	},
+	defaultIntroduction: {
+		"avatar": "/img/avatar.jpg",
+		"name": "blackRain",
+		"github": "https://github.com/heiyu4585",
+		"telegram": "",
+		"qq": "",
+		"bilibili": "",
+		"netease": "",
+		"email": "",
+		"rollText": [
+			"云鹤当归天，天不迎我妙木仙；",
+			"游龙当归海，海不迎我自来也。"
+		],
+		"favorites": [
+			// {
+			// 	"title": "最喜欢的动漫 📺",
+			// 	"content": "异度侵入、春物语、NO GAME NO LIFE、实力至上主义的教室、辉夜大小姐、青春猪头少年不会梦到兔女郎学姐、路人女主、Re0、魔禁、超炮、俺妹、在下坂本、散华礼弥、OVERLORD、慎勇、人渣的本愿、白色相簿2、死亡笔记、DARLING in the FRANXX、鬼灭之刃"
+			// },
+			// {
+			// 	"title": "最喜欢我的女孩子们 🤤",
+			// 	"content": "芙兰达、土间埋、食蜂操祈、佐天泪爷、樱岛麻衣、桐崎千棘、02、亚丝娜、高坂桐乃、五更琉璃、安乐冈花火、一色彩羽、英梨梨、珈百璃、时崎狂三、可儿那由多、和泉纱雾、早坂爱"
+			// },
+			// {
+			// 	"title": "最喜欢玩的游戏 🎮",
+			// 	"content": "Stellaris、巫师、GTA、荒野大镖客、刺客信条、魔兽争霸、LOL、PUBG、耻辱、Screeps"
+			// }
+		]
+
+	}
 }
