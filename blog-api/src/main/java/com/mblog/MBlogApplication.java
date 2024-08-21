f7df93c4bb06a5f0a5f04dbcd7822091c5c6082d
wangning <wangning4567@163.com>
2024-08-21 15:23:34 +0800

    redis

--- blog-api/src/main/java/com/mblog/MBlogApplication.java
+++ blog-api/src/main/java/com/mblog/MBlogApplication.java
@@ -2,12 +2,12 @@ package com.mblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-
+@EnableWebMvc
 @SpringBootApplication
 public class MBlogApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(MBlogApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MBlogApplication.class, args);
+    }
 }
