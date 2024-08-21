f33a56226408b45dcb6e30309daf942fc0b2f5f1
wangning <wangning4567@163.com>
2024-08-21 11:11:27 +0800

    权限

--- blog-api/src/main/java/com/mblog/config/SecurityConfiguration.java
+++ blog-api/src/main/java/com/mblog/config/SecurityConfiguration.java
@@ -20,56 +20,62 @@ import org.springframework.security.config.annotation.authentication.configurati
 @EnableWebSecurity
 public class SecurityConfiguration {
 
-    @Bean
-    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
-        UserDetails user = User.withUsername("user")
-                .password(passwordEncoder.encode("password"))
-                .roles("USER")
-                .build();
+  @Bean
+  public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
+    UserDetails user =
+        User.withUsername("user")
+            .password(passwordEncoder.encode("password"))
+            .roles("USER")
+            .build();
 
-        UserDetails admin = User.withUsername("admin")
-                .password(passwordEncoder.encode("admin"))
-                .roles("USER", "ADMIN")
-                .build();
+    UserDetails admin =
+        User.withUsername("admin")
+            .password(passwordEncoder.encode("admin"))
+            .roles("USER", "ADMIN")
+            .build();
 
-        return new InMemoryUserDetailsManager(user, admin);
-    }
+    return new InMemoryUserDetailsManager(user, admin);
+  }
 
-    // 基础过滤器
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
-                .cors(Customizer.withDefaults()) // 使用默认的CORS配置
-                .authorizeHttpRequests(
-                        authorize -> authorize
-                                .requestMatchers("/login").permitAll()
-                                .requestMatchers(HttpMethod.GET).permitAll()
-                                .anyRequest().authenticated()
-                );
+  // 基础过滤器
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.csrf(csrf -> csrf.disable())
+        .cors(Customizer.withDefaults()) // 使用默认的CORS配置
+        .authorizeHttpRequests(
+            authorize ->
+                authorize
+                    .requestMatchers("/login")
+                    .permitAll()
+                    .requestMatchers(HttpMethod.GET)
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated());
 
-        http.logout((logout) -> logout.permitAll());
+    http.logout((logout) -> logout.permitAll());
 
-        // 设置异常的EntryPoint的处理
-        http.exceptionHandling(exceptions -> exceptions
-                        //                 未登录
-                        .authenticationEntryPoint(new MyAuthenticationEntryPoint())
-                        //                 权限不足
-                        .accessDeniedHandler(new MyAccessDeniedHandler()))
-                //  .addFilter(new JwtAuthenticationFilter(authenticationManager()));
-                .addFilterBefore(new JWTFilter(), LogoutFilter.class);
-        return http.build();
-    }
+    // 设置异常的EntryPoint的处理
+    http.exceptionHandling(
+            exceptions ->
+                exceptions
+                    //                 未登录
+                    .authenticationEntryPoint(new MyAuthenticationEntryPoint())
+                    //                 权限不足
+                    .accessDeniedHandler(new MyAccessDeniedHandler()))
+        //  .addFilter(new JwtAuthenticationFilter(authenticationManager()));
+        .addFilterBefore(new JWTFilter(), LogoutFilter.class);
+    return http.build();
+  }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        return encoder;
-    }
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    return encoder;
+  }
 
-    @Bean
-    public AuthenticationManager authenticationManager(
-            AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
+  @Bean
+  public AuthenticationManager authenticationManager(
+      AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
+  }
 }
